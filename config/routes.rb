e531f1a9da217df0ae1aea370911e284aecd6863
Xiaohan Chen <xiaohanc@usc.edu>
2016-02-25 20:48:59 -0800

    added authentication functions

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  # setting up signup page
+  get 'signup'  => 'users#new'
+  resources :users
+
+  # controller for new user sessions
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
